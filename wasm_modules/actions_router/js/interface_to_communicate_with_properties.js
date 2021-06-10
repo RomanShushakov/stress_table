f41eb1ce9041aee1b4655f52b6afd8b85c6b2883
roman shushakov <roman.a.shushakov1@gmail.com>
2021-06-10 11:41:56 +0300

    unused paths were removed

--- wasm_modules/actions_router/js/interface_to_communicate_with_properties.js
+++ wasm_modules/actions_router/js/interface_to_communicate_with_properties.js
@@ -1,4 +1,3 @@
-// import { communicatorWithProperties } from "/app/web_layout/js/js_modules/communicator_with_properties.js";
 import { communicatorWithProperties } from "../../../../../js/js_modules/communicator_with_properties.js";
 
 export function addMaterialToProperties(actionId, name, youngModulus, poissonRatio, isActionIdShouldBeIncreased) 
